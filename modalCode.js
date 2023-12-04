caff37d59d18a37ddc031600b9e27b39218c99a7
Phillip Lam <plam89@ucla.edu>
2023-12-03 22:23:47 -0800

    admin can now properly edit menu items

--- modalCode.js
+++ modalCode.js
@@ -2,20 +2,28 @@ closeModalBtn.onclick =
     () => myModal.style.display = 'none'
 
 function editItem(e) {
+
   myModal.style.display = 'block'
 
   const chosenItemObj = menuItems.find(item => item.id == e.target.id)
   const {name,description,id,price,emoji} = chosenItemObj
 
   const prevValues = [name,description,price,emoji,id]
-  console.log(prevValues)
   const allInputs = document.querySelectorAll('.modal-content > input')
 
   for (let i = 0; i < allInputs.length; i++) {
     allInputs[i].value = prevValues[i]
   }
 
-}
-function handleEdit(name,description, price, emoji) {
-
+  finalizeEdit.onclick = function() {
+    const name = itemName.value.trim()
+    const description = itemDesc.value.trim()
+    const price = itemPrice.value.trim()
+    const emoji = itemEmoji.value.trim()
+    const obj = {name,description,id,price,emoji}
+          obj.quantity = 1
+    menuItems = menuItems.with(id, obj)
+    myModal.style.display = 'none'
+    renderDefaultMenu()
+  }
 }
